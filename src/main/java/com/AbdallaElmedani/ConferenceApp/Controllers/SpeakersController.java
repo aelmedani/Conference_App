91ce0a98d5a7857fb36ae75a6af08746c84d583b
Abdalla Elmedani <Abdalla.Elmedani@gmail.com>
2019-12-27 11:33:45 -0800

    Created a GET RESTful method for Speakers

--- src/main/java/com/AbdallaElmedani/ConferenceApp/Controllers/SpeakersController.java
+++ src/main/java/com/AbdallaElmedani/ConferenceApp/Controllers/SpeakersController.java
@@ -1,4 +1,32 @@
 package com.AbdallaElmedani.ConferenceApp.Controllers;
 
+import com.AbdallaElmedani.ConferenceApp.Models.Speaker;
+import com.AbdallaElmedani.ConferenceApp.Repositories.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/speakers")
 public class SpeakersController {
+
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> list()
+    {
+        return speakerRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id)
+    {
+        return speakerRepository.getOne(id);
+    }
 }
