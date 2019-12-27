9edb526a9aed6f469cca206a3308849385ff0e91
Abdalla Elmedani <Abdalla.Elmedani@gmail.com>
2019-12-27 11:11:06 -0800

    Creating a Spring REST API session controller.

---
+++ src/main/java/com/AbdallaElmedani/ConferenceApp/Controllers/SessionsController.java
@@ -0,0 +1,30 @@
+package com.AbdallaElmedani.ConferenceApp.Controllers;
+
+import com.AbdallaElmedani.ConferenceApp.Models.Session;
+import com.AbdallaElmedani.ConferenceApp.Repositories.SessionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/sessions")
+public class SessionsController {
+
+    @Autowired
+    private SessionRepository sessionRepository;
+
+    @GetMapping
+    @RequestMapping ("{id}")
+    public List<Session> List() {
+        return sessionRepository.findAll();
+    }
+
+    @PostMapping
+    public Session create(@RequestBody final Session session)
+    {
+        return sessionRepository.saveAndFlush(session);
+    }
+
+
+}
